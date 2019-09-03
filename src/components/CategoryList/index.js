ad0002dcb56ebcb373cb4233ebff73337834f3a7
jeanpier <monserrateluisje@gmail.com>
2019-09-03 16:25:11 -0400

    añadiendo info a las categorias

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -2,13 +2,14 @@ import React from "react"
 
 import { Category } from "../Category"
 import { List, Element} from "./styles"
+import { categories } from "../../../api/db.json"
 
 export const CategoryList = () => {
   return (
     <List>
       {
-        [1,2,3,4].map(element => <Element key={element}>
-            <Category />
+        categories.map(element => <Element key={element.id}>
+            <Category {...element} />
         </Element>)        
       }
     </List>
