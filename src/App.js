aad5ffcdfeee156b1ebfe76a0fb4525f38a1f160
jeanpier <monserrateluisje@gmail.com>
2019-09-03 13:44:58 -0400

    usando styled components

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import { Category } from "./components/Category"
+
+export const App = () => (
+<Category />
+)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
 
-ReactDom.render(<h1>Continuamos con el curso avanzado de reactjs!!!</h1>, document.getElementById("app") )
+import { App } from "./App"
+
+ReactDom.render( <App />, document.getElementById("app") )
