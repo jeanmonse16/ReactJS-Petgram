613c024b1594062f1356343e531379bbc0c76790
jeanpier <monserrateluisje@gmail.com>
2019-09-04 13:03:54 -0400

    creando logo svg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 import { GlobalStyles } from "./GlobalStyles"
+import { Logo } from "./components/Logo"
 import { CategoryList } from "./components/CategoryList"
 import { PhotoCardList } from "./components/PhotoCardList"
 
 export const App = () => (
 <>
   <GlobalStyles />
+  <Logo />
   <CategoryList />
   <PhotoCardList />
 </>
