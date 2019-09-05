2a11a9ce661db165a3cc2f4e80e911bb15a60a3a
jeanpier <monserrateluisje@gmail.com>
2019-09-05 00:11:29 -0400

    añadiendo un useEffect complejo xd

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -1,22 +1,44 @@
-import React, { useEffect, useState } from "react"
+import React, { Fragment, useEffect, useState } from "react"
 
 import { Category } from "../Category"
 import { List, Element} from "./styles"
 
 export const CategoryList = () => {
+
   const [Categories, setCategories] = useState([])
+  const [ShowFixed, setShowFixed] = useState(false)
+
   useEffect( function (){
     window.fetch("https://petgram-api-jeanpier.now.sh/categories")
       .then(res => res.json())
       .then(response => setCategories(response))
   }, [])
+
+  useEffect( function(){
+    const OnScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      ShowFixed != newShowFixed && setShowFixed(newShowFixed) 
+    }
+      document.addEventListener("scroll", OnScroll)
+
+      return () => document.removeEventListener("scroll", OnScroll)
+    }, [ShowFixed]
+  )
+
+  const RenderList = (fixed) => (
+    <List className={fixed ? "fixed" : ""}>
+    {
+      Categories.map(element => <Element key={element.id}>
+          <Category {...element} />
+      </Element>)        
+    }
+  </List>
+  )
+  
   return (
-    <List>
-      {
-        Categories.map(element => <Element key={element.id}>
-            <Category {...element} />
-        </Element>)        
-      }
-    </List>
+    <Fragment>
+      {RenderList()}
+      { ShowFixed && RenderList(true)}
+    </Fragment>
   )
 }
