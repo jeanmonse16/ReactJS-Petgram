6d19edf4dc7ad4946f20e6eb099a1083a9930ccf
jeanpier <monserrateluisje@gmail.com>
2019-09-07 22:00:02 -0400

    añadiendo el polyfill del intersection Observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,43 +1,48 @@
-import React, { Fragment, useEffect, useState, useRef} from "react"
+import React, { Fragment, useEffect, useState, useRef } from 'react'
 
-import { Article, ImgWrapper, Img, Button } from "./styles"
-import { MdFavoriteBorder } from "react-icons/md"
-const DEFAULT_IMAGE =  "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+import { Article, ImgWrapper, Img, Button } from './styles'
+import { MdFavoriteBorder } from 'react-icons/md'
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({
-    id, likes = 0, src = DEFAULT_IMAGE
+  id, likes = 0, src = DEFAULT_IMAGE
 }) => {
-    const element = useRef(null)
-    const [Show, setShow] = useState(false)
+  const element = useRef(null)
+  const [Show, setShow] = useState(false)
 
-    useEffect( function(){
-        const observer = new window.IntersectionObserver( 
-            function(entries){
-              const { isIntersecting } = entries[0]
-              if( isIntersecting ){
-                  setShow(true)
-                  observer.disconnect()
-              }
+  useEffect(function () {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(
+        function (entries) {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            setShow(true)
+            observer.disconnect()
+          }
         })
-        observer.observe(element.current)
-    }, [element])
+      observer.observe(element.current)
+    })
+  }, [element])
 
-    return (
-        <Article ref={element}>
-            {
-               Show &&
-                <Fragment>
-                   <a href={`/detail/${id}`}>
-                    <ImgWrapper>
-                      <Img src={src}/>
-                    </ImgWrapper>
-                   </a>
-                   <Button>
-                     <MdFavoriteBorder size="32px"/>
-                     {likes} likes!
-                   </Button>
-               </Fragment>
-            }
-        </Article>
-    )
+  return (
+    <Article ref={element}>
+      {
+        Show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={src} />
+              </ImgWrapper>
+            </a>
+            <Button>
+              <MdFavoriteBorder size='32px' />
+              {likes} likes!
+            </Button>
+          </>
+      }
+    </Article>
+  )
 }
