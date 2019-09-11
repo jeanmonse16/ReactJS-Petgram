8283a1c9971fbb45960fade9eb65373e9c5d34ce
jeanpier <monserrateluisje@gmail.com>
2019-09-10 23:47:22 -0400

    usando links en vez de hrefs

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React from 'react'
 
-import { Anchor, Image} from "./styles"
-const DEFAULT_IMAGE = "https://i.imgur.com/dJa0Hpl.jpg"
+import { Link, Image } from './styles'
+const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
 
-export const Category = ({cover = DEFAULT_IMAGE, path, emoji = "?"}) => (
-  <Anchor href={path}>
-      <Image src={cover}></Image>
-      {emoji}
-  </Anchor>
+export const Category = ({ cover = DEFAULT_IMAGE, path = '#', emoji = '?' }) => (
+  <Link to={path}>
+    <Image src={cover} />
+    {emoji}
+  </Link>
 )
